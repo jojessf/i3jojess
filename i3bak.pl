11016ac9f13451a082c3b770b905da7cab2e1942
jojess <jojess@yori>
2025-09-11 11:40:01 -0400

    i3bak.pl | add support for local ~/bin scripts matching i3,hdmi ... | uwu

--- i3bak.pl
+++ i3bak.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use Data::Dumper;
 use Sys::Hostname;
 use Getopt::Lazier;
 use File::Path qw(make_path);
@@ -11,16 +12,19 @@ die "mount stuff" if ( ! -d "/nu/inf" );
 $opt->{host} //= hostname;
 $opt->{bakroot} //= "/nu/inf/i3/i3jojess/".$opt->{host}."/";
 mkdir ( $opt->{bakroot} ) if ! -d $opt->{bakroot};
+mkdir ( $opt->{bakroot}."bin" ) if ! -d $opt->{bakroot}."bin";
 $opt->{indir} //= $ENV{HOME};
 #$opt->{bakfils} //= "\.conf,\.jsonc,\.css,\.xml";
-$opt->{bakfils} //= "i3";
+$opt->{bakfils} //= "i3|hdmi"; # if $ENV{DEBUG};";
 $opt->{bakstamp} //= `date +"%Y%m%d%H%M%S"`;
 chomp($opt->{bakstamp});
 
 chdir($opt->{indir}) or die "no indir " . __LINE__ . "\n";
 my @FIs;
+while (<bin/*>) { push(@FIs, $_) }
 while (<*>) { push(@FIs, $_) }
 while (<.*>) { push(@FIs, $_) }
+#print Dumper([ @FIs ])."\n" if $ENV{DEBUG};
 FI: foreach $_ (@FIs) {
    my $skip = 1;
    foreach my $glob (split(",", $opt->{bakfils})) {
@@ -33,15 +37,15 @@ FI: foreach $_ (@FIs) {
       $fidir =~ s/^(.*\/).*?$/$1/;
       $fidir =~ s/^$ENV{HOME}\///g;
       $fipath =~ s/^$ENV{HOME}\///g;
-   print "FI:" . $fi . "\t". $fipath . "\n";   
+   print "FI:" . $fi . "\t". $fipath . "\n" if $ENV{DEBUG};
    if ( -d $fidir ) { 
-      print "MKDIR ".$opt->{bakroot} . $fidir . "\n";
+      print "MKDIR ".$opt->{bakroot} . $fidir . "\n" if $ENV{DEBUG};
       make_path($opt->{bakroot} . $fidir);
       die "$fidir\n" if ! -d $opt->{bakroot} . $fidir;
    }
    next FI if ! -e $fi;
    $fipath = $fi if length($fipath)==0;
-   print "COPY $fipath, ".$opt->{bakroot}.$fipath . "\n";
+   print "COPY $fipath, ".$opt->{bakroot}.$fipath . "\n"; # if $ENV{DEBUG};
    next FI if $opt->{nocp};
    copy($fipath, $opt->{bakroot}.$fipath) or do {
       print "ERROR, next\n";  
